a5cf5d96d73c3243b6d403031912917049876ea2
Mattias Appelgren <mattias@ppelgren.se>
2018-02-01 22:40:17 +0100

    Update builder docker image

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ docker run --rm \
     -v "$PWD":/go/src/githashcrash \
     -w /go/src/githashcrash \
     -e "GOOS=darwin" \
-    golang:1.8 \
+    golang:1.9.3-alpine3.7 \
     go build -o bin/githashcrash-macos
 
 docker run --rm \
     -v "$PWD":/go/src/githashcrash \
     -w /go/src/githashcrash \
     -e "GOOS=linux" \
-    golang:1.8 \
+    golang:1.9.3-alpine3.7 \
     go build -o bin/githashcrash
 
 docker build . -t githashcrash
