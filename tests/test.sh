e259b68070802ddee1ff9f753529bfdd63ca8ce8
Mattias Appelgren <mattias@ppelgren.se>
2019-01-27 21:56:12 +0100

    Cleanup tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
 set -ex
 
+githashcrash=$1
+repo=$2
+testsdir=$(mktemp -d)
 pattern='^00.*'
 
+cleanup() {
+    rm -rf "$testsdir"
+}
+
+trap cleanup EXIT
+
 # Correctness
-cp -r /repo /tmp/repo0 && cd $_
-recreate_cmd=$(githashcrash "$pattern" | tail -1)
-bash -c "$recreate_cmd" &> /dev/null
+cp -r "$repo" "$testsdir/repo0" && cd "$_"
+recreate_cmd=$($githashcrash "$pattern" | tail -1)
+bash -c "$recreate_cmd" &>/dev/null
 hash_0=$(git rev-parse HEAD)
 [[ $hash_0 =~ $pattern ]] || exit 1
 
 # Correctness with seed
-cp -r /repo /tmp/repo1 && cd $_
-recreate_cmd=$(GITHASHCRASH_SEED=11 githashcrash "$pattern" | tail -1)
-bash -c "$recreate_cmd" &> /dev/null
+cp -r "$repo" "$testsdir/repo1" && cd "$_"
+recreate_cmd=$(GITHASHCRASH_SEED=11 GITHASHCRASH_THREADS=1 $githashcrash "$pattern" | tail -1)
+bash -c "$recreate_cmd" &>/dev/null
 hash_1=$(git rev-parse HEAD)
 [[ $hash_1 =~ $pattern ]] || exit 1
 
 # Consistency
-cp -r /repo /tmp/repo2 && cd $_
-recreate_cmd2=$(GITHASHCRASH_SEED=11 githashcrash "$pattern" | tail -1)
-bash -c "$recreate_cmd2" &> /dev/null
+cp -r "$repo" "$testsdir/repo2" && cd "$_"
+recreate_cmd2=$(GITHASHCRASH_SEED=11 GITHASHCRASH_THREADS=1 $githashcrash "$pattern" | tail -1)
+bash -c "$recreate_cmd2" &>/dev/null
 hash_2=$(git rev-parse HEAD)
-diff <(echo $hash_1) <(echo $hash_2)
+diff <(echo "$hash_1") <(echo "$hash_2")
 
 # Recreate in another copy of repo
-cp -r /repo /tmp/repo3 && cd $_
-bash -c "$recreate_cmd" &> /dev/null
+cp -r "$repo" "$testsdir/repo3" && cd "$_"
+bash -c "$recreate_cmd" &>/dev/null
 hash_3=$(git rev-parse HEAD)
-diff <(echo $hash_1) <(echo $hash_3)
+diff <(echo "$hash_1") <(echo "$hash_3")
 
-# Different prefix yield different hash
-cp -r /repo /tmp/repo4 && cd $_
-recreate_cmd4=$(GITHASHCRASH_SEED=22 githashcrash "$pattern" | tail -1)
-bash -c "$recreate_cmd4" &> /dev/null
+# Different seed yield different hash
+cp -r "$repo" "$testsdir/repo4" && cd "$_"
+recreate_cmd4=$(GITHASHCRASH_SEED=22 GITHASHCRASH_THREADS=1 $githashcrash "$pattern" | tail -1)
+bash -c "$recreate_cmd4" &>/dev/null
 hash_4=$(git rev-parse HEAD)
 [[ $hash_4 =~ $pattern ]] || exit 1
-if diff <(echo $hash_1) <(echo $hash_4); then
+if diff <(echo "$hash_1") <(echo "$hash_4"); then
     echo "Expected different hashes"
     exit 1
 fi
 
 # Pass object as argument
-cd /repo
+cp -r "$repo" "$testsdir/repo5.1" && cd "$_"
 obj=$(git cat-file -p HEAD)
-recreate_cmd=$(GITHASHCRASH_SEED=33 githashcrash "$pattern" "$obj" | tail -1)
-cp -r /repo /tmp/repo5
-cd /tmp/repo5
-bash -c "$recreate_cmd" &> /dev/null
+recreate_cmd=$(GITHASHCRASH_SEED=33 $githashcrash "$pattern" "$obj" | tail -1)
+cp -r "$repo" "$testsdir/repo5.2" && cd "$_"
+bash -c "$recreate_cmd" &>/dev/null
 hash_5=$(git rev-parse HEAD)
 [[ $hash_5 =~ $pattern ]] || exit 1
 
